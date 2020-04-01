2ce3fb2d6d11b5cfc628707e62ce64c080e3298b
matt6670 <malder103@gmail.com>
2020-04-01 16:37:26 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,25 +1,44 @@
-// $("form").submit(event){
-//     e.preventDefault()
-//     var query = $("search").val();
-
-// }
-
-
-
-//Needs to send server search data
-//Currently using code from assessment as placeholder
-$("form").submit(event => { 
-    event.preventDefault();
-    axios.post("/search").then(response => {
-      searchData = response.data;
-      $("#searchData").css({
-        "text-align": "center",
-        "min-height": "150px",
-        display: "flex",
-        "justify-content": "center",
-        "align-items": "center"
-      });
-      $("#searchData").html(searchData);
-    });
-  });
-  
+$(document).ready(function(){
+  async function search(){
+      let query = $("#searchBar").val();
+      let result;
+      var API_KEY = "6d0d53e22b308aba126396137fcbabd9";
+      var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web&query=' + query
+      let response ="";
+      var opened;
+      console.log("processing");
+      await $.get(url, function (data) {
+        console.log(data)
+        data.organic_results.forEach(res => {
+            result = `<h1>${res.title}</h1><br><a href="${res.url}">${res.url}</a><p>${res.snippet}</p>`
+            response += result;
+        })
+        console.log("done!");
+    })
+    await $("#data").html("test");
+    newDoc = response;
+    opened = window.open("/search");
+    await opened.document.write(newDoc);
+  }
+  async function lucky(){
+    let query = $("#searchBar").val();
+    let result;
+    var API_KEY = "6d0d53e22b308aba126396137fcbabd9";
+    var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web&query=' + query
+    let response ="";
+    var opened;
+    console.log("processing");
+    await $.get(url, function (data) {
+      console.log(data)
+    var redirecturl = data.organic_results[0].url;
+    window.open(redirecturl);
+  })}
+  $("form button").click(function (ev) {
+    ev.preventDefault()
+    if ($(this).attr("value") == "I'm Feeling Lucky") {
+      lucky();
+    }
+    if ($(this).attr("value") == "Search") {
+      search();
+    }});
+});
