51a8d9e64d274af53015f55303504e7dd615a471
matt6670 <malder103@gmail.com>
2020-04-01 16:28:20 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,66 +1,44 @@
-// $("form").submit(event){
-//     e.preventDefault()
-//     var query = $("search").val();
-
-// }
-
-
-
-//Needs to send server search data
-//Currently using code from assessment as placeholder
-
 $(document).ready(function(){
-
-  async function getAPI(){
-    event.preventDefault();
+  async function search(){
       let query = $("#searchBar").val();
       let result;
       var API_KEY = "6d0d53e22b308aba126396137fcbabd9";
       var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web&query=' + query
       let response ="";
       var opened;
-
       console.log("processing");
       await $.get(url, function (data) {
         console.log(data)
-
-        
-        // im feeling lucky check
-        // if (req.body.lucky = "I'm Feeling Lucky"){
-        //     var redirecturl = data.organic_results[0].url;
-        //     return res.redirect(redirecturl);
-        // };
-        
         data.organic_results.forEach(res => {
             result = `<h1>${res.title}</h1><br><a href="${res.url}">${res.url}</a><p>${res.snippet}</p>`
-
             response += result;
-
         })
-        // res.send(response)
         console.log("done!");
     })
-
     await $("#data").html("test");
     newDoc = response;
     opened = window.open("/search");
     await opened.document.write(newDoc);
-      
   }
-
-
-
-  // var opened = window.open("results.html");
-  // opened.document.write("<html><head><title>MyTitle</title></head><body>test</body></html>");
-  
-
-  
-
-  $("form").submit(event => { 
-
-      getAPI();
-
-    });
+  async function lucky(){
+    let query = $("#searchBar").val();
+    let result;
+    var API_KEY = "6d0d53e22b308aba126396137fcbabd9";
+    var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web&query=' + query
+    let response ="";
+    var opened;
+    console.log("processing");
+    await $.get(url, function (data) {
+      console.log(data)
+    var redirecturl = data.organic_results[0].url;
+    window.open(redirecturl);
+  })}
+  $("form button").click(function (ev) {
+    ev.preventDefault()
+    if ($(this).attr("value") == "I'm Feeling Lucky") {
+      lucky();
+    }
+    if ($(this).attr("value") == "Search") {
+      search();
+    }});
 });
-
-
