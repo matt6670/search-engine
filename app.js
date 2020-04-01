fa804075f477272b111f0bdf17cd155f6fabc794
matt6670 <malder103@gmail.com>
2020-04-01 16:36:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express =require("express");
 const app = express();
 const port = 3000;
-const cors = require("cors");
 
-searchData ="empty";
-
-app.use(cors());
-app.use(express.static("public"));
-
-// Needs to search for and return data posted from client
-app.post("/search", (req, res) => res.send(searchData)); 
-
-
-
-
-
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
